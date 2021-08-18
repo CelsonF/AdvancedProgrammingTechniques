a2763cb6e45cd6f7c121e02e9fa0546ce2d56d2c
CelsonF <celson338540@gmail.com>
2021-08-18 17:27:03 -0300

    implemented programm class

--- TrialAndErrorProgramming/TrialAndError/Program.cs
+++ TrialAndErrorProgramming/TrialAndError/Program.cs
@@ -2,13 +2,37 @@
 
 namespace TrialAndError
 {
+    
+    class WalkHorse 
+    {
+        //Horse movement coordinates
+        int[] dx = {2, 1, -1, -2, -2, -1, 1, 2};
+        int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
+
+        //Number of positions of board
+        int NumberPositions;
+
+        //Number of houses of board
+        int NumberHouses;
+
+        //Board
+        int [,] Board;
+
+        public WalkHorse(int _numberPosition)
+        {
+            NumberPositions = _numberPosition;
+            NumberHouses = NumberPositions * NumberPositions;
+            Board = new int[NumberPositions,NumberPositions]
+        }
+
+
+    }
+    
     class Program
     {
         static void Main(string[] args)
-        {
-           
-
-
+        {             
+          
         }
     }
 }
